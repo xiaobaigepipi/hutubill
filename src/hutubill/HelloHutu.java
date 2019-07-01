bfe79db80d2c67cca4707d7aebca62241ab19303
Administrator <Administrator@PC-20180912JZGQ>
2019-07-01 17:16:26 +0800

    test

--- src/hutubill/HelloHutu.java
+++ src/hutubill/HelloHutu.java
@@ -2,6 +2,6 @@ package hutubill;
 
 public class HelloHutu {
     public static void main(String[] args) {
-        System.out.println("hello hutu");
+        System.out.println("hello hutu344");
     }
 }
